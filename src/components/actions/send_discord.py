416c76f3bac7d26b7cff2766a46d296c143cdccb
blackms <rocchi.b.a@gmail.com>
2023-05-15 19:57:05 +0200

    small change

--- src/components/actions/send_discord.py
+++ src/components/actions/send_discord.py
@@ -1,5 +1,6 @@
 from components.actions import Action
 import logging
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
 class SendToDiscord(Action):
@@ -7,8 +8,21 @@ class SendToDiscord(Action):
         super().__init__()
         self.logger = logging.getLogger(__name__)
 
+    def send_to_discord(self, url, data):
+        discord_webhook = DiscordWebhook(url=url)
+        embed = DiscordEmbed(
+            title='Signal', description='Signal received from webhook', color=242424)
+        discord_webhook.add_embed(embed)
+        response = discord_webhook.execute()
+
     def run(self, *args, **kwargs):
         super().run(*args, **kwargs)  # this is required
         self.logger.info(f"{self.name} ---> SendToDiscord action has run!")
         data = self.validate_data()
+        """
+        foreach discord url in settings:
+            send data to discord
+        """
+        for url in self.settings.DISCORD_WEBHOOKS:
+            self.send_to_discord(url, data)
         print('Data from webhook:', data)
--- src/settings.py
+++ src/settings.py
@@ -9,3 +9,6 @@ REGISTERED_LINKS = [('PrintData', 'WebhookReceived')]
 
 # other configs
 
+DISCORD_WEBHOOKS = [
+    "https://discord.com/api/webhooks/1107723354850471967/WLmfV_8pK-AcG7c9So3tmArVmQevF4nGc2ylhb15XPTvflyWXGCILfjumLU-fZzwwQIw"
+]
